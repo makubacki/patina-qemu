08e977b1e41846ab17c47e7a56a30bfb1fbfd002
Joey Vagedes <joeyvagedes@microsoft.com>
2026-01-28 15:07:15 -0800

    QemuPkg: Move Shell Boot option behind USB and PXE Boot
    Updates the default boot option order to place the UEFI shell behind SSD, USB, and PXE to better support booting to something other then the shell.

--- QemuPkg/Library/MsBootOptionsLibQemu/MsBootOptionsLib.c
+++ QemuPkg/Library/MsBootOptionsLibQemu/MsBootOptionsLib.c
@@ -415,9 +415,9 @@ MsBootOptionsLibRegisterDefaultBootOptions (
   )
 {
   RegisterFvBootOption (&gMsBootPolicyFileGuid, MS_SDD_BOOT, (UINTN)-1, LOAD_OPTION_ACTIVE, (UINT8 *)MS_SDD_BOOT_PARM, sizeof (MS_SDD_BOOT_PARM));
-  RegisterFvBootOption (PcdGetPtr (PcdShellFile), INTERNAL_UEFI_SHELL_NAME, (UINTN)-1, LOAD_OPTION_ACTIVE, NULL, 0);
   RegisterFvBootOption (&gMsBootPolicyFileGuid, MS_USB_BOOT, (UINTN)-1, LOAD_OPTION_ACTIVE, (UINT8 *)MS_USB_BOOT_PARM, sizeof (MS_USB_BOOT_PARM));
   RegisterFvBootOption (&gMsBootPolicyFileGuid, MS_PXE_BOOT, (UINTN)-1, LOAD_OPTION_ACTIVE, (UINT8 *)MS_PXE_BOOT_PARM, sizeof (MS_PXE_BOOT_PARM));
+  RegisterFvBootOption (PcdGetPtr (PcdShellFile), INTERNAL_UEFI_SHELL_NAME, (UINTN)-1, LOAD_OPTION_ACTIVE, NULL, 0);
   RegisterFvBootOption (PcdGetPtr (PcdUIApplicationFile), INTERNAL_UEFI_FP_NAME, (UINTN)-1, LOAD_OPTION_ACTIVE, NULL, 0);
 }
 
